8e4ba079e661a536fcf481f782a45a36157abc2f
Mahdi Esmailiyan <68151089+esmailiyan@users.noreply.github.com>
2020-12-18 22:57:44 +0330

    Create File_Analiser.py

---
+++ Analiser/File_Analiser.py
@@ -0,0 +1,12 @@
+def counter(text,char):
+    count = 0
+    for c in text :
+        if c == char:
+            count += 1
+    return count
+filename = input("Enter your Filename : ")
+with open(filename) as file :
+    text = file.read()
+for char in "abcdefghijklmnopqrstuvwxyz" :
+    perc = 100 * counter(text,char) / len(text)
+    print ("{0} = {1}".format(char,round(perc,2)))
