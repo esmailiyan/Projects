014901fd85c624b60ca89e6783d3a08622f8e17f
Mahdi Esmailiyan <68151089+esmailiyan@users.noreply.github.com>
2020-12-18 19:30:17 +0330

    Create BASE64.py

---
+++ Hash/BASE64.py
@@ -0,0 +1,21 @@
+from base64 import *
+while 1:
+    x=input("You want to 'ENcode' or 'DEcode' or 'EXit'?\n")
+    x=x.lower()
+    if x=='en':
+        ans = b64encode(input("Enter: ").encode('utf-8')).decode()
+        print('----------------------------------')
+        print(ans)
+        print('----------------------------------')
+    elif x == 'de':
+        try:
+            ans = b64decode(input("Enter: ")).decode()
+            print('----------------------------------')
+            print(ans)
+            print('----------------------------------')
+        except:
+            print("Couldn't decode!")
+    elif x=='ex':
+        exit(0)
+    else:
+        print('Try again !!')
